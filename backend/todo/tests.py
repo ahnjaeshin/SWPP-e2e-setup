7417cdc0b10006c2e4ab3e9f73c83cefea51fa09
yuyupopo <yuyupopo@snu.ac.kr>
2018-10-27 22:30:35 +0900

    add simple test backend

--- backend/todo/tests.py
+++ backend/todo/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TodoTestCase(TestCase):
+    def test_index(self):
+        client = Client()
+        response = client.get('/api/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content.decode(), 'Hello World!')
