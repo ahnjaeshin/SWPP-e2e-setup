4eda7893a8dbe5a67950259019b81dbdcdc6ec86
Do Il Yoon <ttotto3080@gmail.com>
2018-10-31 19:52:32 +0900

    implement front

--- frontend/src/app/services/todo.service.ts
+++ frontend/src/app/services/todo.service.ts
@@ -5,8 +5,6 @@ import { Todo } from '../todo';
 import { Observable, of } from 'rxjs';
 import { map, tap, catchError } from 'rxjs/operators';
 
-???????
-
 @Injectable({
   providedIn: 'root'
 })
@@ -14,5 +12,22 @@ export class TodoService {
 
   constructor(private http: HttpClient) { }
 
-  ???????
+  host = "http://localhost:8080"
+
+  getAllTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.host + '/api/todo/');
+  }
+
+  addTodo(todo: Todo) {
+    return this.http.post<Todo>(this.host + '/api/todo', {...todo});
+  }
+
+  updateTodo(todo: Todo) {
+    return this.http.put(this.host + `/api/todo/${todo.id}/`, {...todo});
+  }
+
+  deleteTodoById(todo: Todo) {
+    return this.http.delete(this.host + `/api/todo/${todo.id}/`);
+  }
+
 }
