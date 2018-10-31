ac0aa5d38b38cef316ee2d5971b28c0ebbd42ec7
Jooseung <jsedwardyou@gmail.com>
2018-10-31 19:01:43 +0900

    Initialize

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('todo/', admin.site.urls),
     path('api/', include('todo.urls')),
 ]
--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Todo(models.Model):
+    content = models.CharField(max_length = 120)
+    done = models.BooleanField(default = False)
+
+
 # Create your models here.
