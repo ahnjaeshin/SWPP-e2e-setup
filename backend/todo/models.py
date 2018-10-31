a0366715c8652cf78852e3f5b65fa0eb21067031
Jooseung <jsedwardyou@gmail.com>
2018-10-31 19:30:33 +0900

    Added Todo Model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -4,5 +4,7 @@ class Todo(models.Model):
     content = models.CharField(max_length = 120)
     done = models.BooleanField(default = False)
 
+    def __str__(self):
+        return self.content
 
 # Create your models here.
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
+
+from django.http import HttpResponseBadRequest, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+from json.decoder import JSONDecodeError
+from .models import Todo
 
 
 def index(request):
-    return HttpResponse('Hello World!')
+    return HttpResponse("Hello")
+
+def todo_list(request):
+    if request.method == 'GET':
+        todo_all_list = [todo for todo in Todo.objects.all().values()]
+        return JsonResponse(todo_all_list, safe = False)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
