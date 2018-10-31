07135feab1e13eb46fd6fe394db737e9553ab791
hyeminkim <withkhm@gmail.com>
2018-10-31 20:02:00 +0900

    practice

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('todo/',include('todo.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('todo.urls')),
 ]
