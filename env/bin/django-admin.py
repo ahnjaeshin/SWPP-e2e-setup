14e9c9dec25b9cc15c7868d6bfae3e6864bc6e9d
cobaltblu27 <cobaltblu27@gmail.com>
2018-10-31 20:06:35 +0900

    removed virtualenv

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/cobalt/Desktop/classfiles/software/SWPP-e2e-setup/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
